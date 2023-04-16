429fd21c6a5dbbfb0953291681b07dc3cc64c497
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-04-16 21:43:30 +0200

    Update App.js
    code cleaning and indenting

--- App.js
+++ App.js
@@ -18,36 +18,36 @@ import { useEffect } from "react";
 import { useNetInfo } from "@react-native-community/netinfo"
 
 const App = () => {
-// Defines network connectivity status
-const connectionStatus = useNetInfo();
-  
-// The web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA_PQA7VsKpsPVirhdR5AeGbcT3AXG1haQ",
-  authDomain: "chat-app-53b10.firebaseapp.com",
-  projectId: "chat-app-53b10",
-  storageBucket: "chat-app-53b10.appspot.com",
-  messagingSenderId: "45353717021",
-  appId: "1:45353717021:web:1d6cecf678b597cec0e5f4"
-};
+  // Defines network connectivity status
+  const connectionStatus = useNetInfo();
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // The web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA_PQA7VsKpsPVirhdR5AeGbcT3AXG1haQ",
+    authDomain: "chat-app-53b10.firebaseapp.com",
+    projectId: "chat-app-53b10",
+    storageBucket: "chat-app-53b10.appspot.com",
+    messagingSenderId: "45353717021",
+    appId: "1:45353717021:web:1d6cecf678b597cec0e5f4"
+  };
 
-// Initialize Firestore Database handler
-const db = getFirestore(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Storage handler
-const storage = getStorage(app);
+  // Initialize Firestore Database handler
+  const db = getFirestore(app);
 
-useEffect(() => {
-  if (connectionStatus.isConnected === false){
-    Alert.alert("Connection Lost!");
-    disableNetwork(db);
-  }else if(connectionStatus.isConnected === true){
-    enableNetwork(db);
-  }
-}, [connectionStatus.isConnected]);
+  // Initialize Firebase Storage handler
+  const storage = getStorage(app);
+
+  useEffect(() => {
+    if (connectionStatus.isConnected === false){
+      Alert.alert("Connection Lost!");
+      disableNetwork(db);
+    }else if(connectionStatus.isConnected === true){
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
 
   return (
     <NavigationContainer>
@@ -61,16 +61,16 @@ useEffect(() => {
         <Stack.Screen
           name="Chat"
         >
-          {props => <Chat
-            isConnected={connectionStatus.isConnected}
-            db={db}
-            storage={storage}
-            {...props}
-          />}
+        {props => <Chat
+          isConnected={connectionStatus.isConnected}
+          db={db}
+          storage={storage}
+          {...props}
+        />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-export default App;
+  export default App;
